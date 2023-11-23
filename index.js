b16a584c150dee92e7b478f865dbeea4d4d126cb
rodriguezmauricio <contato.mauricioalves@gmail.com>
2023-11-23 23:18:18 +0000

    calculadora de rankeadas

---
+++ index.js
@@ -0,0 +1,56 @@
+// Instruções para entrega
+//  # 2️⃣ Calculadora de partidas Rankeadas
+// **O Que deve ser utilizado**
+
+// - Variáveis
+// - Operadores
+// - Laços de repetição
+// - Estruturas de decisões
+// - Funções
+
+// ## Objetivo:
+
+// Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
+// depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+
+// Se vitórias for menor do que 10 = Ferro
+// Se vitórias for entre 11 e 20 = Bronze
+// Se vitórias for entre 21 e 50 = Prata
+// Se vitórias for entre 51 e 80 = Ouro
+// Se vitórias for entre 81 e 90 = Diamante
+// Se vitórias for entre 91 e 100= Lendário
+// Se vitórias for maior ou igual a 101 = Imortal
+
+// ## Saída
+
+// Ao final deve se exibir uma mensagem:
+// "O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
+
+let finalResult = calcRanked(500, 10);
+let saldoVitorias = finalResult[0];
+let nivel = finalResult[1];
+
+function calcRanked(wins, losses) {
+  let result = wins - losses;
+  let rank = "";
+
+  if (result <= 10) {
+    rank = "Ferro";
+  } else if (result > 10 && result <= 20) {
+    rank = "Bronze";
+  } else if (result > 20 && result <= 50) {
+    rank = "Prata";
+  } else if (result > 50 && result <= 80) {
+    rank = "Ouro";
+  } else if (result > 80 && result <= 90) {
+    rank = "Diamante";
+  } else if (result > 90 && result <= 100) {
+    rank = "Lendário";
+  } else {
+    rank = "Imortal";
+  }
+
+  return [result, rank];
+}
+
+console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`);
